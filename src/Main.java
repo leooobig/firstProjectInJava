885385fe81fb7ac097916972b55085ce62ba8336
Leonardo Lima <leonardolimastos2003@gmail.com>
2026-05-01 10:45:08 -0300

    declarando variáveis

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        // Declaração de variáveis
+        int anoDoLancamento = 2002;
+        System.out.println("Ano de lançamento: " + anoDoLancamento);
+
+        boolean incluidoNoPlano = true;
+
+        double notaDoFilme = (9.5 + 6.2 + 8.4) / 3;
+        System.out.println(notaDoFilme);
+
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura
+                Muito Bom!
+                Ano de Lançamento:
+                """ + anoDoLancamento;
+        System.out.println(sinopse);
 
     }
 }
