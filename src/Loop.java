895476f7542ddb58f2a6630d714c7662ced86962
Leonardo Lima <leonardolimastos2003@gmail.com>
2026-05-02 09:28:39 -0300

    implementando media de avaliação com for

---
+++ src/Loop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Diga sua avaliação do filme de 0 a 10: ");
+            nota = leitura.nextDouble();
+            mediaAvaliacao += nota;
+        }
+
+        System.out.println("Média da avaliação: " + mediaAvaliacao/3);
+
+    }
+}
